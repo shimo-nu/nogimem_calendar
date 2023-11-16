bbf1450637972716735ab68aae17e55fd3f07a56
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2023-11-16 20:57:34 +0900

    function that add calendar to database

--- nogi_scrayping.py
+++ nogi_scrayping.py
@@ -15,6 +15,7 @@ import datetime
 import argparse
 from dotenv import load_dotenv
 from rich.console import Console
+from rich.progress import Progress
 
 # DB access
 from nogi_db import NogiDB
@@ -28,7 +29,7 @@ options.add_argument('--headless')
 options.add_argument('--disable-extensions')
 options.add_argument('--lang=ja-JP')
 
-def getNogimember():
+def setNogimember():
   
 
 
@@ -76,6 +77,97 @@ def getNogimember():
   
   nogidb.insert_data(data, "user")
   
+def setNogiCalendar():
+  calendar_selector = "#md-sch > div.md--sc__in"
+  
+  with NogiDB("TEST.db") as nogidb:
+    nogi_members = nogidb.get_all_user(nogidb.db_connect)
+  
+  with Progress() as progress:
+    task1 = progress.add_task("[red] Add Schedule to database...", total = len(nogi_members)+1)
+    for member in nogi_members:
+      progress.update(task1, advance=1, description=f"[red] Add Schedule to database...{member[1]}")   
+      ## webdriver
+      driver_mem = webdriver.Chrome(options=options)
+      driver_mem.implicitly_wait(10)
+      # driver_mem.set_window_size('1200', '1000')
+      driver_mem.get(member[2])
+      # element = driver_mem.find_element(By.CSS_SELECTOR, value=".js-lang-swich.ja")
+      # print(element.is_displayed())
+      # element.click()
+      ## wait
+      time.sleep(3)
+
+      member_article_soup = BeautifulSoup(driver_mem.page_source, 'html.parser')
+
+      # driver_mem.quit()
+
+      calendar_dict = {}
+      for i in member_article_soup.select(calendar_selector):
+        contents = i.find_all('div', class_="sc--day")
+        
+        content_list = []
+        for content in contents:
+            day = content.find('p', class_="sc--day__d").text
+            category = content.find('p', class_="m--scone__cat__name")
+            time_duration = content.find('p', class_="m--scone__start")
+            title = content.find('p', class_="m--scone__ttl") 
+            url = content.find('a', class_="m--scone__a").get("href")
+
+            
+            article ={}
+
+            if (category is not None):
+                article["category"] = category.text
+            else:
+                article["category"] = "no category"
+            if (time_duration is not None):
+                article["duration"] = time_duration.text
+            if (title is not None):
+                article["title"] = title.text
+            else:
+                article["title"] = "no title"
+            if (url is not None):
+                article["url"] = url
+                match_txt = re.search(r'wd00=(\d{4}).*wd01=(\d{2}).*wd02=(\d{2})', url)
+
+                if match_txt:
+                    year, month, day = match_txt.groups()
+                if "duration" in article:
+                    start_time_str, end_time_str = article["duration"].split("〜")
+
+                    start_hour, start_minute = map(int, start_time_str.split(":"))
+                    if (end_time_str != ""):
+                        end_hour, end_minute = map(int, end_time_str.split(":"))
+                    else:
+                        end_hour, end_minute = start_hour + 1, end_minute
+
+                    day_offset_start = start_hour // 24
+                    day_offset_end = end_hour // 24
+                    start_hour %= 24
+                    end_hour %= 24
+                    year, month, day = int(year), int(month), int(day)
+                    st_datetime = datetime.datetime(year, month, day, start_hour, start_minute) + datetime.timedelta(days=day_offset_start)
+                    et_datetime = datetime.datetime(year, month, day, end_hour, end_minute) + datetime.timedelta(days=day_offset_end)
+                    start_time = f"{st_datetime.year}-{st_datetime.month}-{st_datetime.day}-{st_datetime.hour}-{st_datetime.minute}"
+                    end_time = f"{et_datetime.year}-{et_datetime.month}-{et_datetime.day}-{et_datetime.hour}-{et_datetime.minute}"
+                    date = f"{year}-{month}-{day}"
+                    article["date"] = [date, start_time, end_time]
+                else:
+                    date_only = f"{year}-{month}-{day}"
+                    article["date"] = [date_only, 0, 0]
+
+            content_list.append(article)
+        with NogiDB("TEST.db") as nogidb:
+          for content in content_list:
+
+            date, st_time, end_time = content["date"]
+            calendar_data = (member[0], date, st_time, end_time, content["category"], content["title"])
+            nogidb.insert_user_calendar(nogidb.db_connect, calendar_data)
+        
+
+  
   
 if __name__=='__main__':
-  getNogimember()
+  setNogimember()
+  setNogiCalendar()
